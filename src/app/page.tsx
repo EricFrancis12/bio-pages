504cfe32606ed4df934de4feba1cb7bf62d32fc6
Eric <ericfrancis1@proton.me>
2024-03-04 21:00:58 -0600

    metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,8 @@ import { Metadata } from 'next';
 import Home from './lib/components/Home/Home';
 
 export const metadata: Metadata = {
-    title: 'Dashboard'
+    title: 'Rybio.link | Link In Bio Page-Builder',
+    description: 'Rybio.link | Link In Bio Page-Builder'
 };
 
 export default async function Page() {
